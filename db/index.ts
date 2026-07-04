128b44b02befff7b0409b75e37b72896398a624a
amamads <amir.1rpm@gmail.com>
2026-07-04 19:44:20 +0330

    save

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,32 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+// export const db = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
 
-export default db;
+// export default db;
+
+import { PrismaPg } from "@prisma/adapter-pg";
+
+// Initialize the adapter according to your driver's requirements
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+// Pass the adapter instance to PrismaClient
+const prisma = new PrismaClient({ adapter });
+
+// Find all users with their posts
+const users = await prisma.user.findMany({
+  include: { posts: true },
+});
+
+// Create a user with a post
+const user = await prisma.user.create({
+  data: {
+    email: "alice@prisma.io",
+    posts: {
+      create: { title: "Hello World" },
+    },
+  },
+});
--- prisma/migrations/20260611151700_todos/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "todo" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "isDone" BOOLEAN NOT NULL,
-    "text" TEXT NOT NULL
-);
